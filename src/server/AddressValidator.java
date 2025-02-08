2ab610a56a3380c2b2d4c6bc28c700b338c03ed5
Amir Mishayev <127884315+amir3x0@users.noreply.github.com>
2025-02-08 20:32:07 +0200

    api work

--- src/server/AddressValidator.java
+++ src/server/AddressValidator.java
@@ -3,6 +3,6 @@ package server;
 public class AddressValidator {
     // בדוק אם הכתובת בפורמט ביטקוין בסיסי (1... או 3...)
     public boolean isValid(String address) {
-        return address.matches("^[13][a-km-zA-HJ-NP-Z1-9]{25,34}$");
+    	return address.matches("^(1[a-km-zA-HJ-NP-Z1-9]{25,34}|3[a-km-zA-HJ-NP-Z1-9]{25,34}|bc1[a-zA-HJ-NP-Z0-9]{11,80})$");
     }
 }
